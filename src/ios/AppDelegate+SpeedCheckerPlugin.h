963836e7e86d6fa2ae9b7e5afd17512f235a5427
Murod Khodzhaev <murod.hodjaev@gmail.com>
2024-02-09 16:18:29 +0300

    add test demo app

--- src/ios/AppDelegate+SpeedCheckerPlugin.h
+++
@@ -1,7 +0,0 @@
-#import "AppDelegate.h"
-
-@interface AppDelegate (SpeedCheckerPlugin) <UIApplicationDelegate>
-- (BOOL)getBackgroundNetworkTestingEnabled;
-- (void)setBackgroundNetworkTestingEnabled:(BOOL)testsEnabled;
-+ (AppDelegate *_Nonnull) instance;
-@end
